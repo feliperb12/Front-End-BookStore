d2b57e392a477eef1369ac7546dad0d5b80727fd
feliperb12 <feliperibeiroaraujo3@hotmail.com>
2022-08-19 17:07:08 -0300

    Criado o Update para Livros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { CategoriaUpdateComponent } from './components/views/categoria/categoria
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/views/home/home.component';
+import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 
 
 const routes: Routes = [
@@ -39,6 +40,10 @@ const routes: Routes = [
   {
     path:'categorias/:id_cat/livros/create',
     component:LivroCreateComponent
+  },
+  {
+    path:'categorias/:id_cat/livros/:id/update',
+    component:LivroUpdateComponent
   }
 ];
 
--- src/app/components/views/livro/livro-update/livro-update.component.ts
+++ src/app/components/views/livro/livro-update/livro-update.component.ts
@@ -4,20 +4,20 @@ import { Route, Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
-@Component({
-  selector: 'app-livro-create',
-  templateUrl: 'livro-create.component.html',
-  styleUrls: ['./livro-create.component.css']
-})
-export class LivroCreateComponent implements OnInit {
 
+@Component({
+  selector: 'app-livro-update',
+  templateUrl: 'livro-update.component.html',
+  styleUrls: ['./livro-update.component.css']
+})
+export class LivroUpdateComponent implements OnInit {
   id_cat: String = '';
 
   livro: Livro = {
-    id:'',
-    titulo:'',
-    nome_autor:'',
-    texto:''
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
   }
 
   titulo = new FormControl("", [Validators.minLength(3)])
@@ -28,36 +28,46 @@ export class LivroCreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+    this.livro.id = this.route.snapshot.paramMap.get('id')!
+    this.findById()
   }
-  create():void{
-    this.service.create(this.livro, this.id_cat).subscribe((resposta)=>{
-      this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.service.mensagem('Livro criado com sucesso!');
-    }, err =>{
-      this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.service.mensagem('Erro ao criar novo livro.Tente mais tarde!');
+
+  findById(): void {
+    this.service.findById(this.livro.id!).subscribe((resposta) => {
+      this.livro = resposta
     })
   }
-  getMessage(){
-    if(this.titulo.invalid){
+
+  update(): void {
+    this.service.update(this.livro).subscribe((resposta) => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Livro atualizado com suceso!')
+    }, err =>{
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Falha ao atualizar o livro. Tente novamente..');
+    })
+  }
+
+  getMessage() {
+    if (this.titulo.invalid) {
       return 'O campo TITULO deve conter entre 3 a 100 caracteres';
     }
     return false;
   }
-  getMessageAutor(){
-      if(this.nome_autor.invalid){
+  getMessageAutor() {
+    if (this.nome_autor.invalid) {
       return 'O campo AUTOR deve conter entre 3 a 100 caracteres';
     }
     return false;
   }
-  getMessageTexto(){
-    if(this.texto.invalid){
+  getMessageTexto() {
+    if (this.texto.invalid) {
       return 'O campo Texto deve conter entre 10 a 200000 caracteres';
+    }
+    return false;
   }
-  return false;
-}
-    
-  cancel():void{
+
+  cancel(): void {
     this.router.navigate([`categorias/${this.id_cat}/livros`])
   }
 
